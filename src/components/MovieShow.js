1fe0c7757d90bfd9961d376c283a7bb8427ac4b3
MisterC1ean <robert.keller44@icloud.com>
2020-05-25 12:50:09 -0400

    Done.

---
+++ src/components/MovieShow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieShow = ({ match, movies }) => {
+  
+  return (
+    <div>
+      <h3>{ movies[match.params.movieId].title }</h3>
+    </div>
+  );
+}
+
+export default MovieShow;
